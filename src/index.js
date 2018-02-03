4bd0f6b322c51c4bb0ab55ec51016c057a1472d0
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-02-03 12:29:33 +1100

    Begin Project with clean template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = function() {
+    return <div>Hi there</div>;
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
