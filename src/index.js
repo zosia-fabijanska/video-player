b1c341f8e2c1743589faa82c8730dd011fe8d5d3
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-02-03 13:41:20 +1100

    Add component files and add API key

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const API_KEY = 'AIzaSyCWruVTa5LMxvrzilJJXOw6hGGstbAX54s';
+
 const App = function() {
     return <div>Hi there</div>;
 }
