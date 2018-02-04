5d8e1c25f1b7909caea53036022c722186b36cf9
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-02-04 17:06:03 +1100

    added styled components

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
     console.log(video);
 
     const videoURL = video.snippet.thumbnails.default.url;
     const videoTitle = video.snippet.title;
+
+    const mainImage = styled.img`
+        border: 2px solid black;
+    `
     return (
-        <li className="list-group-item">
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
                     <img className="media-object" src={videoURL} alt="video thumbnail"/>
