64835be380ef095256c57f0e427ddd62f82735d9
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-02-04 11:47:05 +1100

    Call You Tube API and store data in state in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCWruVTa5LMxvrzilJJXOw6hGGstbAX54s';
 
-const App = () => {
-return (
-    <div>
-        <SearchBar />
-    </div>);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {videos:[]};
+        
+        YTSearch({key: API_KEY, term: 'design'}, (videos) => {
+            this.setState({videos});
+        })
+    }
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>);
+    };
 }
 // Generate HTML from the component and render it in the the DOM
 ReactDOM.render(<App/>, document.querySelector('.container'));
